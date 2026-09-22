d2d62a3d25d5125cd3a7d238ba0a5049fa8cc118
HachiYoku <6531503142@lamduan.mfu.ac.th>
2026-09-23 01:08:04 +0700

    add production monitoring and safe error handling

--- apps/Backend/config/dbConnection.js
+++ apps/Backend/config/dbConnection.js
@@ -1,19 +1,9 @@
 const mongoose = require('mongoose');
 
-
 const connectDB = async () => {
-   try {
-       const connect = await mongoose.connect(process.env.MONGO_DB)
-       console.log(
-           'Database connected: ',
-           connect.connection.host,
-           connect.connection.name
-       )
-   } catch (err) {
-       console.log(err)
-       process.exit(1)
-   }
-}
+  await mongoose.connect(process.env.MONGO_DB);
+  console.log("Database connected");
+};
 
 
 module.exports = connectDB
